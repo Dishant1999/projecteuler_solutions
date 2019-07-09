442dfae1f72b6a1e3059e602fc0836adcd885f1d
Dishant1999 <43281715+Dishant1999@users.noreply.github.com>
2019-07-09 19:56:06 +0530

    Add files via upload

---
+++ largest_prime_factor.py
@@ -0,0 +1,14 @@
+def find(A):
+    if A == 1:
+        print(" 1 doesnt have any prime factors")
+    i=2
+    while i <=A:
+        if A%i==0:
+            A = A/i
+            print(i)
+            i=1
+        i=i+1
+
+
+find(5666789004566788)
+
---
+++ sum_mul_3_5.py
@@ -0,0 +1,17 @@
+def func_three(y):
+    if y==0:
+        # print y
+        return y
+    else:
+        if y%3==0:
+            print "if"
+            print y
+	    return y+func_three(y-1)
+        else:
+            print "else"
+	    print y
+            return func_three(y-1)
+
+
+result=func_three(1000)
+print result
